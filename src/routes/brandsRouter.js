e7ec638f101d3e0a73f117d50c8aff668f0deb34
yara <yaralviana@gmail.com>
2021-12-15 18:12:32 -0300

    adicionando rotas

--- src/routes/brandsRouter.js
+++ src/routes/brandsRouter.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require('../controller/brandsController')
 
-
+router.get('/all', controller.getAll)
+router.get('/:id', controller.getId)
 
 module.exports = router
