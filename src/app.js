054856700a7ab406a0383cf6c67cd6523baf7a39
yara <yaralviana@gmail.com>
2021-12-15 18:38:58 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 
+require('dotenv-safe').config()
 const db = require('./database/mongoConfig')
+
+const brandsRoutes = require('./routes/brandsRoutes')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use("/brands", brandsRoutes)
 
 db.connect() 
 
--- src/controller/brandsController.js
+++ src/controller/brandsController.js
@@ -1,10 +1,10 @@
-const brandSchema = require('../models/brandSchema')
+const brandSchema = require('../models/brandsSchema')
 const mongoose = require('mongoose');
 
 // GET ALL
 const getAll = async (req, res) => {
     try {
-        const brandFound = await brandSchema.find();
+        const brandFound = await brandsSchema.find();
         res.status(200).send(brandFound);
     } catch (error) {
         res.status(500).send(error.message);
@@ -13,15 +13,32 @@ const getAll = async (req, res) => {
 // GET ONE
 const getId = async (req, res) => {
     try {
-        const brandFound = await brandSchema.findById(req.params.id);
+        const brandFound = await brandsSchema.findById(req.params.id);
         res.status(200).json(brandFound);
     } catch (error) {
         res.status(500).send(error.message);
     }
 }
+// Adicionando função de like
+const like = (req, res) => {
+
+    const { id } = req.params;
+    const found = models.find(brands => brands.id == id);
+    if (found == undefined) {
+
+        res.status(404).send({message: "Empresa/marca não encontrada."});
+        
+    }
+    found.likes += 1;
+
+    res.status(200).send(found);
+
+}
+
 
 
 module.exports = {
     getAll,
-    getId
+    getId,
+    like
 }
