9dda2e47ef904055bbe97368fbfe2e799bf9453b
yara <yaralviana@gmail.com>
2021-12-15 18:40:47 -0300

    wip

--- src/routes/brandsRoutes.js
+++ src/routes/brandsRoutes.js
@@ -4,5 +4,6 @@ const controller = require('../controller/brandsController')
 
 router.get('/all', controller.getAll)
 router.get('/:id', controller.getId)
+router.patch("/:id/like", controller.like);
 
 module.exports = router
